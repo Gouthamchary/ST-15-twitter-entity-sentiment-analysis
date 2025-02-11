c617c26129a3e4e04f56fb37de52e15cdde997a4
goutham <gouthamchary456@gmail.com>
2025-02-11 12:35:07 +0530

    hi

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 from test import TextToNum
 import pickle
-
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/predict",methods=["POST","Get"])
+
+@app.route("/predict",methods=['POST','GET'])
 def predict():
     if request.method=="POST":
         msg=request.form.get("message")
@@ -18,16 +18,29 @@ def predict():
         ob.token()
         ob.removeStop()
         st=ob.stemme()
-        stem_vector=' '.join(st)    
+        stem_vector=" ".join(st)
+
         with open("vectorizer.pickle","rb") as vc:
             vectorizer=pickle.load(vc)
         vcdata=vectorizer.transform([stem_vector]).toarray()
         print(vcdata)
+        
+        with open("model.pickle","rb") as mc:
+            model=pickle.load(mc)
 
-        with open("model.pickle","rb") as md:
-            model=pickle.load(md)
+        pred=model.predict(vcdata)
+        print(pred)
+        sentiment_map = {1: "Positive 😊", 0: "Neutral 😐", -1: "Negative 😢"}
+        sentiment = sentiment_map.get(pred[0], "Unknown")
+
+        return render_template("result.html",sentiment=sentiment)
+       
+
+
+
     else:
         return render_template("predict.html")
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5050)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port='5050')
